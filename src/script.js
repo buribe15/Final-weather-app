15d4725efd0cfb4fc0f6548d01c63ad8c81bc051
buribe15 <84052226+buribe15@users.noreply.github.com>
2022-02-13 17:20:16 -0700

    Finished CSS style & unit conversion

--- src/script.js
+++ src/script.js
@@ -33,7 +33,8 @@ function displayTemp(response) {
   let dateElm = document.querySelector("#date");
   let iconElm = document.querySelector("#icon");
 
-  tempElm.innerHTML = Math.round(response.data.main.temp);
+  metricTemp = Math.round(response.data.main.temp);
+  tempElm.innerHTML = metricTemp;
   cityElm.innerHTML = response.data.name;
   descripElm.innerHTML = response.data.weather[0].description;
   windElm.innerHTML = Math.round(response.data.wind.speed);
@@ -58,7 +59,32 @@ function submitForm(event) {
   search(citySearchElm.value);
 }
 
+function displayImperial(event) {
+  event.preventDefault();
+  let imperialTemp = (metricTemp * 9) / 5 + 32;
+  let tempElm = document.querySelector("#temperature");
+  tempElm.innerHTML = Math.round(imperialTemp);
+  metricLink.classList.remove("active");
+  imperialLink.classList.add("active");
+}
+
+function displayMetric(event) {
+  event.preventDefault();
+  let tempElm = document.querySelector("#temperature");
+  tempElm.innerHTML = metricTemp;
+  metricLink.classList.add("active");
+  imperialLink.classList.remove("active");
+}
+
 let form = document.querySelector("#search-box");
 form.addEventListener("submit", submitForm);
+
+let imperialLink = document.querySelector("#imperial");
+imperialLink.addEventListener("click", displayImperial);
+
+let metricLink = document.querySelector("#metric");
+metricLink.addEventListener("click", displayMetric);
+
+let metricTemp = null;
 
 search("Seattle");
